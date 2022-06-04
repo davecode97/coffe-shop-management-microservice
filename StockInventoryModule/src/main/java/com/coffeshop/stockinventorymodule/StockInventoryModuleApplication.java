ff886f894c108a0cd2f3b61a3b6f34bbcfeb95df
David Llanes <llanesdavid97@gmail.com>
2022-06-04 17:07:46 -0500

    CSM-3: Stock Module

---
+++ StockInventoryModule/src/main/java/com/coffeshop/stockinventorymodule/StockInventoryModuleApplication.java
@@ -0,0 +1,13 @@
+package com.coffeshop.stockinventorymodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInventoryModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockInventoryModuleApplication.class, args);
+    }
+
+}
---
+++ StockInventoryModule/src/test/java/com/coffeshop/stockinventorymodule/StockInventoryModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeshop.stockinventorymodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInventoryModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
